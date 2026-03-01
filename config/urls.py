c7bff8cc01da16e029cfd769f510d7ea7f4c3983
Ibrohim2442 <ibrohimavazov731@gmail.com>
2026-03-01 17:32:41 +0500

    Add orders URL pattern to project configuration

--- config/urls.py
+++ config/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     path("api/cart/", include("apps.cart.urls")),
 
     path("api/reviews/", include("apps.reviews.urls")),
+    path("api/orders/", include("apps.orders.urls")),
 
     # YOUR PATTERNS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
